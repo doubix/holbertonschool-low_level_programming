02dc5a1bc7123e4ccd0a7406f18c430ad190a45e
root <adib.grouz@gmail.com>
2019-09-18 11:55:56 +0100

    fix whitespaces

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * print meaasge
+ * display message onscreen
  * Return: Always 0 (Success)
  */
-
-int main(void) 
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");	
-    return (0);
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
