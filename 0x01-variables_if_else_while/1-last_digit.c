c1ee3ceb1724c36b8aada4098f788e4c89d6d9cc
root <adib.grouz@gmail.com>
2019-09-19 13:46:48 +0100

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* betty style doc for function main goes there */
+/* more headers goes there */
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+/* betty style doc for function main goes there */
+
 int main(void)
 {
 int n, ln;
