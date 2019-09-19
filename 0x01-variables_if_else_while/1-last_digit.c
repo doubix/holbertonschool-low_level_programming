ff32652cfe46b467059fe324a2a752e5fb9682eb
root <adib.grouz@gmail.com>
2019-09-19 13:20:11 +0100

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 last_digit = (n % 10);
 if (last_digit > 5)
 {
-printf("Last digit of %d is %d and is grater than 5\n", n, last_digit);
+printf("Last digit of %d is greater than 5\n", n);
 }
 else if (last_digit < 6 && last_digit != 0)
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is less than 6 and not 0\n", n);
 }
 else
 {
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
+printf("Last digit of %d is 0\n", n);
 }
 return (0);
 }
