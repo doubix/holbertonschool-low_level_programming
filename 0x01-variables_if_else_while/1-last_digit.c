a726e02819203636133cb41afc0c44e8f578b516
root <adib.grouz@gmail.com>
2019-09-19 14:04:17 +0100

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,11 +24,11 @@ printf("Last digit of %d is %d and is greater than 5\n", n, ln);
 }
 else if (ln == 0)
 {
-printf("Last digit of %d ia %d  and is 0\n", n, ln);
+printf("Last digit of %d ia %d and is 0\n", n, ln);
 }
 else
 {
-printf("Last digit of %d is %d and is  less than 6 and not 0\n", n, ln);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ln);
 }
 return (0);
 }
