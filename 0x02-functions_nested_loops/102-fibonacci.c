86dcf53850af527f4c48474f9c1292fed84911c3
root <adib.grouz@gmail.com>
2019-09-24 13:33:17 +0100

    advc

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - print fibonacci
- *
+ * fibonacci s
  * Return: always 0
  */
 int main(void)
@@ -11,7 +11,7 @@ int ip = 0;
 
 printf("%ld, ", u0);
 printf("%ld, ", u1);
-for (ip = 1; ip < 50; ip++)
+for (ip = 1; ip < 48; ip++)
 {
 printf("%ld, ", u0 + u1);
 u1 = u0 + u1;
