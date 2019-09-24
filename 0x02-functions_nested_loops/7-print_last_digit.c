273c8512eff3115f60e05ccee1571758b474594b
root <adib.grouz@gmail.com>
2019-09-24 10:45:29 +0100

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
  * print_last_digit - entry point
- * @c: number to check
+ * @c: number to  check
  * Return: Always 0 (Success)
  */
 int print_last_digit(int c)
 {
-_putchar(c % 10 + '0');
-return (c % 10);
+c %= 10;
+_putchar(c + '0');
+return (c);
 }
