4db16c4964031fe8881efe96182af9a36bbf0e7a
root <adib.grouz@gmail.com>
2019-09-24 12:33:31 +0100

    advc

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,10 +19,12 @@ _putchar(' ');
 if (i * j < 10)
 {
 _putchar(' ');
+_putchar(' ');
 _putchar(i * j  + '0');
 }
 else if  (i * j >= 10 && i * j < 100)
 {
+_putchar(' ');
 _putchar(i * j / 10 + '0');
 _putchar(i * j % 10 + '0');
 }
