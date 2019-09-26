cd1d17584ea8772fd3e83a0683f6e4de94b2d593
root <adib.grouz@gmail.com>
2019-09-26 17:08:20 +0100

    fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-    long n = 612852475143, i = 2;
+long n = 612852475143, i = 2;
 
-    while (i * i < n)
-    {
-         while (n % i == 0)
-            n = n / i;
-        i = i + 1;
-         
-    }
-    printf("%ld\n", n);
-    return 0;
+while (i * i < n)
+{
+while (n % i == 0)
+n = n / i;
+i = i + 1;
+}
+printf("%ld\n", n);
+return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 /**
  * print_number - prints the largest prime factor of number 612852475143
- *
+ * @n: number
  * Return: 0;
  */
 void print_number(int n)
 {
-int size =1;
+int size = 1;
 
-if (n<0)
+if (n < 0)
 n = -n;
 
 while (size < n)
 size *= 10;
 
-if (size > 10) 
+if (size > 10)
 size /= 10;
 
 while (size)
 {
 _putchar(n / size + '0');
-n%=size;
-size/=10;
+n %= size;
+size /= 10;
 }
 }
