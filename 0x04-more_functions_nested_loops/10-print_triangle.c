2c2a7396ccced0bcbd4eb107dce7393676451330
root <adib.grouz@gmail.com>
2019-09-26 14:13:38 +0100

    fix output

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,11 @@ int i, j;
 
 if (n > 0)
 {
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
-if (j < n - i)
+if (j < n - i - 1)
 _putchar(' ');
 else
 _putchar('#');
@@ -27,5 +27,4 @@ else
 {
 _putchar('\n');
 }
-
 }
