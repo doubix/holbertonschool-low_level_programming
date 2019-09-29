4beb9bb6363787930ea9db4d0ba7faa8a5e79981
root <adib.grouz@gmail.com>
2019-09-29 13:16:11 +0100

    max

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,30 +7,16 @@
  */
 void print_number(int n)
 {
-int size = 100000000, r;
+   if (n < 0) {
+       putchar('-');
+       n = -n;
+   }
 
-if (n < 0)
-{
-n = -n;
-_putchar('-');
-}
-r = n % 10;
-n=n/10;
-if (r < 0)
-r = -r;
+   if (n == 0)
+      putchar('0');
 
-if (n < 10)
-size = 1;
-else
-{
-while (n / size == 0)
-size /= 10;
-}
-while (size > 1)
-{
-_putchar(n / size + '0');
-n %= size;
-size /= 10;
-}
-_putchar('0' + r);
+   if (n/10)
+      print_number(n/10);
+
+   putchar(n%10 + '0');
 }
