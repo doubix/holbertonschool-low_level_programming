f8c685e1f513c16442a356fdedd549e6f01dc41f
root <adib.grouz@gmail.com>
2019-10-01 14:21:07 +0100

    fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,19 +9,19 @@
 
 int _atoi(char *s)
 {
-int i, n, signe = 1;
+	int i, n, signe = 1;
 
-i = n = 0;
-while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
-{
-i++;
-if (*(s + i) == '-')
-signe *= -1;
-}
-while ((*(s + i) >= '0') && (*(s + i) <= '9'))
-{
-n = n * 10 + (*(s + i) - '0');
-i++;
-}
-return (signe *n);
+	i = n = 0;
+	while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	{
+		if (*(s + i) == '-')
+			signe *= -1;
+		i++;
+	}
+	while ((*(s + i) >= '0') && (*(s + i) <= '9'))
+	{
+		n = n * 10 + signe * (*(s + i) - '0');
+		i++;
+	}
+	return (n);
 }
