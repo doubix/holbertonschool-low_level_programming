984a13e0e394730bea3c37faead541533dff65ac
root <adib.grouz@gmail.com>
2019-10-01 01:21:53 +0100

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,8 +10,8 @@
 void rev_string(char *s)
 {
 int i, n;
-
 char c;
+
 for (n = 0; s[n]; n++)
 ;
 for (i = 0; i < n / 2; i++)
@@ -20,5 +20,4 @@ c = s[i];
 s[i] = s[n - i - 1];
 s[n - i - 1] = c;
 }
-_putchar('\n');
 }
