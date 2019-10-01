32679f25107cae65b3e0e1823a990ef20ebb5fe7
root <adib.grouz@gmail.com>
2019-10-01 01:53:46 +0100

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,13 @@ void puts2(char *str)
 {
 int i = 0;
 
-while (*(str + i))
+for (i = 0; str[i] != 0; i++)
 {
-_putchar(*(str + i));
-i = i + 2;
+if (i % 2 == 0)
+{
+str[i];
+_putchar(i);
+}
 }
 _putchar('\n');
 }
