838cf126ebe82ddd10ae40696fff65ddac268330
root <adib.grouz@gmail.com>
2019-10-23 16:08:30 +0100

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,14 +9,15 @@
  * Return: first index for which cmp doesn't return 0, or -1
  */
 
-int int_index(int *array, int size, int (*cmp)(int)){
+int int_index(int *array, int size, int (*cmp)(int))
+{
 int i;
 if (size > 0 && array && cmp)
 {
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
-return(i);
+return (i);
 }
 }
 return (-1);
