4d128a5c6cd3f74f6d7689d0ee62bc3cbb78f610
doubix <adib.grouz@gmail.com>
2019-10-31 15:32:58 +0100

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,12 +7,11 @@
 
 size_t list_len(const list_t *h)
 {
+const list_t *tmp;
 unsigned int i;
 
-while (h)
-{
-h = h->next;
-i++;
-}
+tmp = h;
+for (i = 0; tmp; i++)
+tmp = tmp->next;
 return (i);
 }
