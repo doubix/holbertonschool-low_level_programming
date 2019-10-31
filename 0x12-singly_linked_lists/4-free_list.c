d3611a07896de411dfcc0dc2362a7c3fdb10168e
doubix <adib.grouz@gmail.com>
2019-10-31 15:53:23 +0100

    fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include"lists.h"
 /**
  * free_list - frees a list
@@ -5,14 +6,13 @@
  */
 void free_list(list_t *head)
 {
-list_t *tmp, *next;
+list_t *tmp;
 
-tmp = head;
-while (tmp != NULL)
+while (head)
 {
-next = tmp->next;
+tmp = head;
+head = head->next;
 free(tmp->str);
 free(tmp);
-tmp = next;
 }
 }
