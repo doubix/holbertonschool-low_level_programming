f0534bbddd64359a0f0eab555cf8ee127416cda6
doubix <adib.grouz@gmail.com>
2019-12-16 23:38:59 +0100

    ftets

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/johnconnor77/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/0x17/jackpot.so 
+wget -P ./ https://github.com/s0m35h1t/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/win.so 
 export LD_PRELOAD=./win.so
