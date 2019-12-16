292d2e10f588821ef3dfbebe1273c4188a3e93d6
doubix <adib.grouz@gmail.com>
2019-12-16 23:44:14 +0100

    bfix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ./ https://github.com/s0m35h1t/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=./win.so
+wget -P /tmp https://github.com/s0m35h1t/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
