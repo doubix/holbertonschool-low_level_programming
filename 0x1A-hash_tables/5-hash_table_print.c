23901838e80599e08fc21fa7b272c3d36c1a8aa8
doubix <adib.grouz@gmail.com>
2020-01-19 18:29:22 +0100

    [fix] betty style

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,25 +7,25 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned int long i = 0;
-    hash_node_t *tmp;
-    int flag = 0;
+unsigned int long i = 0;
+hash_node_t *tmp;
+int flag = 0;
 
-    if (ht == NULL || ht->array == NULL)
-        return;
-    printf("{");
-    while (i < ht->size)
-    {
-            tmp = ht->array[i];
-            while (tmp)
-            {
-                if (flag)
-                    printf(", ");
-                printf("'%s': '%s'", tmp->key, tmp->value);
-                flag = 1;
-                tmp = tmp->next;
-            }
-        i++;
-    }
-    printf("}\n");
+if (ht == NULL || ht->array == NULL)
+return;
+printf("{");
+while (i < ht->size)
+{
+tmp = ht->array[i];
+while (tmp)
+{
+if (flag)
+printf(", ");
+printf("'%s': '%s'", tmp->key, tmp->value);
+flag = 1;
+tmp = tmp->next;
+}
+i++;
+}
+printf("}\n");
 }
