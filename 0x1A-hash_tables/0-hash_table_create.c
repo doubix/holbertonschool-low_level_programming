fb29b7fc9a6fb1798b7f772d581c0c888d7a18a6
doubix <adib.grouz@gmail.com>
2020-01-19 17:34:33 +0100

    [update] return NULL when size 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *ht;
 
-ht = malloc(sizeof(hash_table_t));
-if (!ht || size == 0)
+if (size == 0)
 return (NULL);
-ht->array = calloc(size, sizeof(hash_node_t));
+
+ht = malloc(sizeof(hash_table_t));
+if (!ht)
+return (NULL);
+ht->array = malloc(size * sizeof(hash_node_t));
 if (!ht->array)
 {
 free(ht);
