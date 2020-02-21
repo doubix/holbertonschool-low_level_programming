7dfc60ab82b62e3949d5a05708f5e7ee5515ff54
doubix <adib.grouz@gmail.com>
2020-02-21 22:20:38 +0100

    [update] condition

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,8 +23,8 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 perimeter += 4
-                if grid[i][j - 1] is 1 and j - 1 >= 0:
+                if grid[i][j - 1] == 1 and j - 1 >= 0:
                     perimeter -= 2
-                if grid[i - 1][j] is 1 and i - 1 >= 0:
+                if grid[i - 1][j] == 1 and i - 1 >= 0:
                     perimeter -= 2
     return perimeter
