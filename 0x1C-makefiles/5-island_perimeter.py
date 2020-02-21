7811ad54fa02c9964ee64a12fb6dc551cac336e0
doubix <adib.grouz@gmail.com>
2020-02-21 22:16:19 +0100

    [update] grid validator

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,6 +12,10 @@ def island_perimeter(grid):
     Retruns:
         (int)
     """
+    if not grid or not grid[0]:
+        return
+    if type(grid) is not list or type(grid[0]) is not list:
+        return
     width = len(grid[0])
     heigth = len(grid)
     perimeter = 0
