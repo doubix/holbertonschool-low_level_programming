d1e8d71fb0cd33a0c3b12468732b5b74fd43efd0
doubix <adib.grouz@gmail.com>
2020-04-17 00:05:48 +0100

    [fix] update cond

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,7 +25,9 @@ int jump_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-	jump = jump < size - 1 ? jump : size - 1;
+	if (jump >= size) 
+		jump = size -1;
+
 	for (; i < jump && array[i] < value; i++)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
